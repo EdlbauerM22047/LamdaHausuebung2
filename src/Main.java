f8f0051dfae617f00d608ae7248c11df8f30b14a
EdlbauerM22047 <EdlbauerM22047@sus.htl-grieskirchen.at>
2024-09-27 22:17:32 +0200

    Leere Klasse herausgelöscht und testFile-Mothode begonnen

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        //NumberTester numberTester=new NumberTester("numbers.txt");
+        NumberTester numberTester=new NumberTester("numbers.txt");
+        numberTester.testFile();
 
     }
 }
--- src/NumberTester.java
+++ src/NumberTester.java
@@ -1,10 +1,13 @@
+import java.io.*;
+
 public class NumberTester {
+    String fileName;
     NumberTest oddTester;
     NumberTest primeTester;
     NumberTest palidromeTester;
 
     public NumberTester(String fileName){
-
+        this.fileName=fileName;
     }
     public void setOddEvenTester(NumberTest oddTester){
         this.oddTester=oddTester;
@@ -18,6 +21,28 @@ public class NumberTester {
     }
 
     public void testFile(){
-        
+
+        int testfaelle;
+        try {
+            BufferedReader bufferedReader=new BufferedReader(new FileReader(new File(fileName)));
+            testfaelle=Integer.parseInt(bufferedReader.readLine());
+
+            for(int i=1;i<testfaelle;i++){
+                String line= bufferedReader.readLine();
+                String []splittedLine=line.split(" ");
+                int wasWirdUeberprueft=Integer.parseInt(splittedLine[0]);
+                int zahlMitDerGerechnetWird=Integer.parseInt(splittedLine[1]);
+
+                switch (wasWirdUeberprueft){
+                    case 1:setOddEvenTester((n)->n%2==0);
+                        System.out.println(oddTester );break;
+                    case 2:
+                    case 3:
+
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Fehler mit BufferdReadeer");
+        }
     }
 }
