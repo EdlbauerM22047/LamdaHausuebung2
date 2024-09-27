adc0e00cfaef3feeda3d435672bd5af7cdd606d0
EdlbauerM22047 <EdlbauerM22047@sus.htl-grieskirchen.at>
2024-09-27 14:57:23 +0200

    setter und Konstuktor und Variablen erstellt

--- src/NumberTester.java
+++ src/NumberTester.java
@@ -1,2 +1,32 @@
 public class NumberTester {
+
+    NumberTester oddTester;
+    NumberTester primeTester;
+
+    NumberTester palindromeTester;
+
+    public void setPalindromeTester(NumberTester palindromeTester) {
+        this.palindromeTester = palindromeTester;
+    }
+
+    public NumberTester(NumberTester oddTester, NumberTester primeTester, NumberTester palindromeTester) {
+        this.oddTester = oddTester;
+        this.primeTester = primeTester;
+        this.palindromeTester = palindromeTester;
+    }
+
+    public void setOddEvenTester (NumberTester oddTester ) {
+        this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTester primeTester){
+        this.primeTester=primeTester;
+    }
+
+    public void testFile(){
+        
+    }
+
+
+
 }
