7e9360a58caf854632a4713414f9463ccc14521e
EdlbauerM22047 <EdlbauerM22047@sus.htl-grieskirchen.at>
2024-09-27 15:48:31 +0200

    Tester-Methoden gemacht

--- src/NumberTester.java
+++ src/NumberTester.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class NumberTester {
 
 
@@ -5,6 +10,16 @@ public class NumberTester {
 
     public void setPalindromeTester(NumberTest palindromeTester) {
 
+        palindromeTester=(n)->{
+            String nstring=""+n;
+            char[] chars=nstring.toCharArray();
+            for(int i=0;i<nstring.length();i++){
+                if(chars[i]!=chars[chars.length-1-i]){
+                    return false;
+                }
+            }
+            return false;
+        };
     }
 
     public NumberTester(String filename) {
@@ -12,17 +27,25 @@ public class NumberTester {
     }
 
     public void setOddEvenTester (NumberTest oddTester ) {
+            oddTester =(n)->(n%2)==0;
 
     }
 
     public void setPrimeTester(NumberTest primeTester){
-        
+        primeTester=(n)-> isPrime(n);
     }
 
     public void testFile(){
-
+        
     }
 
-
+    public  boolean isPrime(int n){
+        for(int i=2;i<=n/2;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
