e23e1d2008f8b7667d070f25292aac8aad0aa54f
EdlbauerM22047 <EdlbauerM22047@sus.htl-grieskirchen.at>
2024-09-27 21:47:41 +0200

    NumberTester nochmal neu angefangt weil ich mich nicht mehr ausgekannt habe

--- src/NumberTester.java
+++ src/NumberTester.java
@@ -1,51 +1,23 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-
 public class NumberTester {
+    NumberTest oddTester;
+    NumberTest primeTester;
+    NumberTest palidromeTester;
 
-
-    String filename;
-
-    public void setPalindromeTester(NumberTest palindromeTester) {
-
-        palindromeTester=(n)->{
-            String nstring=""+n;
-            char[] chars=nstring.toCharArray();
-            for(int i=0;i<nstring.length();i++){
-                if(chars[i]!=chars[chars.length-1-i]){
-                    return false;
-                }
-            }
-            return false;
-        };
-    }
-
-    public NumberTester(String filename) {
+    public NumberTester(String fileName){
 
     }
-
-    public void setOddEvenTester (NumberTest oddTester ) {
-            oddTester =(n)->(n%2)==0;
-
+    public void setOddEvenTester(NumberTest oddTester){
+        this.oddTester=oddTester;
     }
-
     public void setPrimeTester(NumberTest primeTester){
-        primeTester=(n)-> isPrime(n);
+        this.primeTester=primeTester;
+    }
+
+    public void setPalidromeTester(NumberTest palidromeTester){
+        this.palidromeTester=palidromeTester;
     }
 
     public void testFile(){
         
     }
-
-    public  boolean isPrime(int n){
-        for(int i=2;i<=n/2;i++){
-            if(n%i==0){
-                return false;
-            }
-        }
-        return true;
-    }
-
 }
--- src/PrimeSieve.java
+++
@@ -1,3 +0,0 @@
-public interface PrimeSieve {
-   public boolean isPrime(int n);
-}
